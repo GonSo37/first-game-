4cc7c84673aa15071a20cfb71a307b3dc25593ae
Venera73 <96598254+Venera73@users.noreply.github.com>
2023-04-22 22:06:35 +0200

    Add files via upload

--- first_game_pvp.cpp
+++ first_game_pvp.cpp
@@ -1,5 +1,5 @@
 ﻿#include <iostream>
-
+#include <ctime>
 using namespace std;
 class Animals
 {
@@ -14,19 +14,30 @@ public:
     {
         return "0";
     };
+    virtual double GetHP()
+    {
+        return 0;
+    }
 
 };
 class Dog : public Animals
 {
+    double HP = 5 + rand() % 20;
     string name = "Dog";
 public:
     string glos() override
     {
+        
         return "Gaw";
     }
     string GetName() override
     {
+
         return this->name;
+    }
+    double GetHP()
+    {
+        return this->HP;
     }
 };
 class Cat : public Animals
@@ -58,43 +69,57 @@ public:
 };
 void Object(Animals& object)
 {
-    cout << "You want take " << object.GetName() << "?" << endl;
 
     cout << object.GetName() << " make sounds: " << object.glos() << "!" << endl;
+    cout << "Your pet have " << object.GetHP() << " heat points" << endl;
 
 }
 int main()
 {
-    Dog dog;
-    Cat cat;
-    Bird bird;
-    Animals* chooseObject;
-    cout << "choose your animal: " << endl
-        << "1) " << dog.GetName() << endl
-        << "2) " << cat.GetName() << endl
-        << "3) " << bird.GetName() << endl;
+    srand(time(0));
+    const int sizeOfPl = 2;
+    Dog dog[sizeOfPl];
+    Cat cat[sizeOfPl];
+    Bird bird[sizeOfPl];
+    Animals* chooseObject[sizeOfPl];
+    for (int i = 0; i < sizeOfPl; i++)
+    {
+        chooseObject[i] = 0;
+    }
+    cout << "FARM FITHER" << endl << endl;
+    cout << "choose your animal: " << endl;
+       cout << "1) " << dog[0].GetName() << endl
+        << "2) " << cat[0].GetName() << endl
+        << "3) " << bird[0].GetName() << endl;
     int numAn;
+    cout << "===========================" << endl;
+    for (int i = 0; i < sizeOfPl; i++)
+    {
+        cout << "Player #" << i + 1 << endl;
+        cout << "Write a number of animal: "; cin >> numAn;
+        while (numAn >= 4 || numAn <= 0)
+        {
+            cout << "You wrote the wrong number!" << endl
+                << "Try again: "; cin >> numAn;
+        }
+        if (numAn == 1)
+        {
+            cout << "You took " << dog[i].GetName() << endl;
+            chooseObject[i] = &dog[i];
+        }
+        else if (numAn == 2) {
+            cout << "You took " << cat[i].GetName() << endl;
+            chooseObject[i] = &cat[i];
+        }
+        else if (numAn == 3)
+        {
+            cout << "You took " << bird[i].GetName() << endl;
+            chooseObject[i] = &bird[i];
+        }
+        Object(*chooseObject[i]);
+        
+        
+    }
 
-    cout << "Write a number of animal: "; cin >> numAn;
-    while (numAn >= 4 || numAn <= 0)
-    {
-        cout << "You wrote the wrong number!" << endl
-            << "Try again: "; cin >> numAn;
-    }
-    if (numAn == 1)
-    {
-        cout << "you took " << dog.GetName() << endl;
-        chooseObject = &dog;
-    }
-    else if (numAn == 2) {
-        cout << "You took " << cat.GetName() << endl;
-        chooseObject = &cat;
-    }
-    else if (numAn == 3)
-    {
-        cout << "You took " << bird.GetName() << endl;
-        chooseObject = &bird;
-    }
-    Object(&chooseObject);
     return 0;
 }
